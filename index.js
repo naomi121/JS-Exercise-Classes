418cc8513a2550204fd1fc73d03938c2e53afcb3
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2019-11-14 16:06:00 -0500

    adding

--- index.js
+++ index.js
@@ -41,7 +41,13 @@ class Airplane {
 */
 
 class Person {
- constructor
+ constructor(name, age) {
+   this.name = name;
+   this.age = age,
+   this.stomach = []
+ }
+let 
+
 }
 
 /*
@@ -92,10 +98,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+ constructor(attr){
+   super(attr);
+   this.specialty = attr.specialty,
+   this.favLanguage = attr.favLanguage,
+   this.catchPhrase - attr.catchPhrase
+  }
+  demo(subject){
+  return `Today we are learning about ${subject}`
 }
-
+grade(student, subject){
+  return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
